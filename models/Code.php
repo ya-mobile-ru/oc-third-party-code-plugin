4437d66223902c7c1b601fa1568ca0754163673e
evgenydmorozov <evgenydmorozov@gmail.com>
2022-09-02 11:25:15 +0500

    Adds code functionality

---
+++ models/Code.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Yamobile\ThirdPartyCode\Models;
+
+use Model;
+
+/**
+ * Model
+ */
+class Code extends Model
+{
+    use \October\Rain\Database\Traits\Validation;
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'yamobile_thirdpartycode_codes';
+
+    /**
+     * @var array Validation rules
+     */
+    public $rules = [
+        'name' => 'required',
+        'is_enabled' => 'required',
+        'code' => 'required',
+        'target_id' => 'required',
+    ];
+
+    public $belongsTo = [
+        'target_id' => '\Yamobile\ThirdPartyCode\Models\Targets',
+    ];
+}
