07666292e6e04b92e954dc71dfe55f80dee0f7c5
evgenydmorozov <evgenydmorozov@gmail.com>
2023-04-17 16:24:12 +0500

    feat: add localization for codes

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -17,4 +17,60 @@ return [
             ],
         ],
     ],
+    'code' => [
+        'name' => 'Codes',
+        'pages' => [
+            'index' => [
+                'title' => 'Codes',
+                'breadcrumb' => 'Codes',
+            ],
+            'preview' => [
+                'title' => 'Code preview',
+                'breadcrumb' => 'Code preview',
+            ],
+            'create' => [
+                'title' => 'Code create',
+                'breadcrumb' => 'Code create',
+            ],
+            'update' => [
+                'title' => 'Code update',
+                'breadcrumb' => 'Code update',
+            ],
+            'reorder' => [
+                'title' => 'Code reorder',
+                'breadcrumb' => 'Code reorder',
+            ],
+        ],
+        'fields' => [
+            'id' => [
+                'label' => 'ID',
+            ],
+            'name' => [
+                'label' => 'Name',
+                'comment' => 'Displays in admin panel only',
+                'placeholder' => 'Third party service',
+            ],
+            'is_enabled' => [
+                'label' => 'Enabled',
+                'comment' => 'Output code',
+            ],
+            'code' => [
+                'label' => 'Code',
+                'comment' => 'HTML-code',
+            ],
+            'created_at' => [
+                'label' => 'Created at',
+            ],
+            'updated_at' => [
+                'label' => 'Updated at',
+            ],
+            'target' => [
+                'label' => 'Target',
+                'comment' => 'In which component to display the code',
+            ],
+            'sort_order' => [
+                'label' => 'Order',
+            ],
+        ],
+    ],
 ];
--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -17,4 +17,60 @@ return [
             ],
         ],
     ],
+    'code' => [
+        'name' => 'Код',
+        'pages' => [
+            'index' => [
+                'title' => 'Код',
+                'breadcrumb' => 'Код',
+            ],
+            'preview' => [
+                'title' => 'Просмотр кода',
+                'breadcrumb' => 'Просмотр кода',
+            ],
+            'create' => [
+                'title' => 'Создание кода',
+                'breadcrumb' => 'Создание кода',
+            ],
+            'update' => [
+                'title' => 'Обновление кода',
+                'breadcrumb' => 'Обновление кода',
+            ],
+            'reorder' => [
+                'title' => 'Сортировка кода',
+                'breadcrumb' => 'Сортировка кода',
+            ],
+        ],
+        'fields' => [
+            'id' => [
+                'label' => 'ID',
+            ],
+            'name' => [
+                'label' => 'Название',
+                'comment' => 'Отображается только в админ панели',
+                'placeholder' => 'Сторонний сервис',
+            ],
+            'is_enabled' => [
+                'label' => 'Включено',
+                'comment' => 'Отображать код',
+            ],
+            'code' => [
+                'label' => 'Код',
+                'comment' => 'HTML-код',
+            ],
+            'created_at' => [
+                'label' => 'Создано',
+            ],
+            'updated_at' => [
+                'label' => 'Обновлено',
+            ],
+            'target' => [
+                'label' => 'Цель',
+                'comment' => 'В каких компонентах отображать код',
+            ],
+            'sort_order' => [
+                'label' => 'Порядок',
+            ],
+        ],
+    ],
 ];
